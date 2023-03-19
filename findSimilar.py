ac281080a85313bcd813b073ddb8f2d314f894bd
Emin Can ÖZGE <emincanozge@outlook.com>
2023-03-19 14:20:37 +0300

    Update

--- findSimilar.py
+++ findSimilar.py
@@ -1,8 +1,10 @@
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import numpy as np
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 import tensorflow as tf
+from PIL import Image
 
 # Veri setinin bulunduğu klasör
 data_dir = 'posters780'
@@ -18,7 +20,7 @@ with open('features.pickle', 'rb') as f:
 model = tf.keras.applications.VGG19(include_top=False, pooling='avg')
 
 # Benzer görüntüleri bulmak için bir fonksiyon yazın
-def find_similar_images(image_path, n=20):
+def find_similar_images(image_path, n=5):
     # Görüntüyü yükle
     img = tf.keras.preprocessing.image.load_img(image_path, target_size=img_size)
     x = tf.keras.preprocessing.image.img_to_array(img)
@@ -38,7 +40,11 @@ def find_similar_images(image_path, n=20):
     for i, idx in enumerate(similar_indices):
         filename = os.listdir(data_dir)[idx]
         print(f'{i+1}. {filename} - similarity: {similarities[idx]}')
-
-
-find_similar_images('deneme/tt0024216.jpg')
+        im = Image.open("posters780/"+filename)
+        im.show()
+file_path=""
+while file_path!="exit":
+	file_path=input("File name: ")
+	if file_path!="exit":
+		find_similar_images("posters780/"+file_path+".jpg")
 
