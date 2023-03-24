852065fdeca0d3914047dcf5e63ffc4e53ee7f06
Emin Can ÖZGE <emincanozge@outlook.com>
2023-03-24 22:31:35 +0300

    Update

--- movieRecommendation/movieRecommendationAPI/urls.py
+++ movieRecommendation/movieRecommendationAPI/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from movieRecommendationAPI import views
 
 urlpatterns = [
-    path("", views.get_similar_images)
+    path('', views.root_page),
+    path('GetSimilarPosters', views.get_similar_images)
 ]
--- movieRecommendation/movieRecommendationAPI/views.py
+++ movieRecommendation/movieRecommendationAPI/views.py
@@ -4,9 +4,13 @@ import os
 import numpy as np
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
+
+
+@api_view(["GET"])
+def root_page(request):
+    return Response("Root page")
 
 
 @api_view(["GET"])
@@ -20,18 +24,18 @@ def get_similar_images(request):
     return Response(find_similar_images(movie_id, amount + 1))
 
 
-data_dir = 'posters780'
+data_dir = '../posters780'
 
 img_size = (300, 300)
 
-with open('features.pickle', 'rb') as f:
+with open('../features.pickle', 'rb') as f:
     features = pickle.load(f)
 
 model = tf.keras.applications.VGG19(include_top=False, pooling='avg')
 
 
 def find_similar_images(image_path, n=5):
-    img = tf.keras.preprocessing.image.load_img("posters780/" + image_path + ".jpg", target_size=img_size)
+    img = tf.keras.preprocessing.image.load_img("../posters780/" + image_path + ".jpg", target_size=img_size)
     x = tf.keras.preprocessing.image.img_to_array(img)
     x = tf.keras.applications.vgg19.preprocess_input(x)
     x = np.expand_dims(x, axis=0)
