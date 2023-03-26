25624b256e6f419343437cce099677d669a6b4b3
Emin Can ÖZGE <emincanozge@outlook.com>
2023-03-26 23:16:55 +0300

    Change GET request with POST

--- movieRecommendation/movieRecommendation/settings.py
+++ movieRecommendation/movieRecommendation/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-&3kmfpubuiy*)ukhc(^h!cfifngs(vw6a-vhes-5xf8bgp*t9%
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'movieRecommendation.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -112,7 +107,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- movieRecommendation/movieRecommendationAPI/views.py
+++ movieRecommendation/movieRecommendationAPI/views.py
@@ -1,11 +1,14 @@
+import os
+import pickle
+
+import numpy as np
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import os
-import numpy as np
-import pickle
 from sklearn.metrics.pairwise import cosine_similarity
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 
 
 @api_view(["GET"])
@@ -13,15 +16,34 @@ def root_page(request):
     return Response("Root page")
 
 
-@api_view(["GET"])
+# @api_view(["GET"])
+# def get_similar_images(request):
+#     movie_id = request.GET.get("movie_id")
+#     amount = int(request.GET.get("amount"))
+#     if movie_id is None:
+#         return Response()
+#     if amount is None:
+#         amount = 10
+#     return Response(find_similar_images(movie_id, amount + 1))
+
+
+@api_view(["POST"])
 def get_similar_images(request):
-    movie_id = request.GET.get("movie_id")
-    amount = int(request.GET.get("amount"))
-    if movie_id is None:
-        return Response()
-    if amount is None:
-        amount = 10
-    return Response(find_similar_images(movie_id, amount + 1))
+    try:
+        data = request.data.dict()
+        if "movie_id" in data:
+            movie_id = data["movie_id"]
+            if "amount" in data:
+                amount = int(data["amount"])
+                return Response(find_similar_images(movie_id, amount + 1))
+            else:
+                return Response(find_similar_images(movie_id))
+        else:
+            return Response(False)
+    except AttributeError:
+        return Response("Movie ID not provided.")
+    except Exception:
+        return Response("Error occured.")
 
 
 data_dir = '../posters780'
@@ -34,7 +56,7 @@ with open('../features.pickle', 'rb') as f:
 model = tf.keras.applications.VGG19(include_top=False, pooling='avg')
 
 
-def find_similar_images(image_path, n=5):
+def find_similar_images(image_path, amount=5):
     img = tf.keras.preprocessing.image.load_img("../posters780/" + image_path + ".jpg", target_size=img_size)
     x = tf.keras.preprocessing.image.img_to_array(img)
     x = tf.keras.applications.vgg19.preprocess_input(x)
@@ -44,7 +66,7 @@ def find_similar_images(image_path, n=5):
 
     similarities = cosine_similarity(query_feature, features)[0]
 
-    similar_indices = similarities.argsort()[::-1][:n]
+    similar_indices = similarities.argsort()[::-1][:amount]
 
     res = list()
     for i, idx in enumerate(similar_indices):
