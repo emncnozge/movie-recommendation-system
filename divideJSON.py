4e33d70d093cfd298ed0e278202bf1fe39db818b
Emin Can ÖZGE <emincanozge@outlook.com>
2023-04-25 16:34:03 +0300

    Divide JSON file into info and reviews

---
+++ divideJSON.py
@@ -0,0 +1,31 @@
+import json
+
+
+with open('final_data.json', 'r', encoding="utf-8") as f:
+    data = json.load(f)
+
+
+movieInfo = []
+movieReviews = []
+
+
+for movie in data:
+    movieInfo.append({
+        "imdb_id": movie["imdb_id"],
+        "title": movie["title"],
+        "poster_path": movie["poster_path"],
+    })
+
+    movieReviews.append({
+        "tmdb_id": movie["tmdb_id"],
+        "imdb_id": movie["imdb_id"],
+        "reviews": movie["reviews"]
+    })
+
+
+with open('movie_info.json', 'w') as f:
+    json.dump(movieInfo, f, indent=4, sort_keys=False)
+
+
+with open('movie_reviews.json', 'w') as f:
+    json.dump(movieReviews, f, indent=4, sort_keys=False)
